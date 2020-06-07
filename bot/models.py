05c0e77c5ef6e70be0ba8bbc42f8f70ec8e1e555
Your Name <azat.nur@gmail.com>
2020-06-07 09:07:17 +0200

    self error resolved

--- bot/models.py
+++ bot/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     first_name  = models.CharField(max_length=64)
     last_name   = models.CharField(max_length=64)
 
-    def __str__(str):
+    def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
 class Message(models.Model):

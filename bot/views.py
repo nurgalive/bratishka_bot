f15f5d293b82461126806913e30a64533e9cf539
Your Name <azat.nur@gmail.com>
2020-06-07 09:20:15 +0200

    Local JSON test passed

--- bot/views.py
+++ bot/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
+import json
+from bot.models import User, Message
+from datetime import datetime
 
 def index(request):
     return HttpResponse("Hello, world. This is the bot app.")
